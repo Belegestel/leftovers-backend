59712d07229acfe1a8dbd264e278da6a865ec41c
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-06-16 10:59:00 +0200

    chore: fixed the file structure

--- src/app.module.ts
+++ src/app.module.ts
@@ -2,7 +2,7 @@ import { ConfigModule } from '@nestjs/config';
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { UsersModule } from './users.module';
+import { UsersModule } from './users/users.module';
 
 @Module({
   imports: [
