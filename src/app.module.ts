f8b3a81b3db19f5550587e4d51f582fdfe7b2260
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-06-17 11:23:38 +0200

    feat: /auth/signup/ endpoint

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,13 +3,15 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { UsersModule } from './users/users.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
     ConfigModule.forRoot({
       isGlobal: true,
     }),
-    UsersModule
+    UsersModule,
+    AuthModule
   ],
   controllers: [AppController],
   providers: [AppService],
