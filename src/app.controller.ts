28782bd67ae9f22378c528461453be6c8d64d993
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-06-17 09:09:13 +0200

    feat: Swagger API docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,3 +1,4 @@
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 import { Controller, Get } from '@nestjs/common';
 import { AppService } from './app.service';
 
@@ -5,6 +6,8 @@ import { AppService } from './app.service';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
+  @ApiOperation({ summary: 'Check if the server is running.' })
+  @ApiResponse({ status: 200, description: 'Server is running.' })
   @Get('health')
   getHealth(): string {
     return "OK";
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { UsersService } from './users.service';
+import { ApiOperation, ApiResponse } from '@nestjs/swagger';
 
 @Controller()
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiOperation({ summary: 'Returns a list of users in the database.' })
+  @ApiResponse({ status: 200, description: 'List of users: ID, name and email.' })
   @Get()
   findAll() {
     return this.usersService.findAll();
