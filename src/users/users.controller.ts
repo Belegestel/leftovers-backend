62256085542d99c746552abd6db3a2934b1a335f
Wojciech Dudziński <76164171+Belegestel@users.noreply.github.com>
2026-06-17 10:16:28 +0200

    feat: swagger docs
    * feat: .env file config support
    
    * feat: added prisma integration
    
    * feat: added prisma migrations
    
    * chore: fixed the file structure
    
    * chore: added database URL in the prisma schema
    
    * chore: added users repository
    
    * chore: fixed prisma migration chaos
    
    * feat: Swagger API docs
    
    * chore: improved Swagger docs

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,10 +1,22 @@
-import { Controller, Get } from '@nestjs/common';
-import { UsersService } from './users.service';
+import { Controller, Get } from "@nestjs/common";
+import { UsersService } from "./users.service";
+import { ApiOperation, ApiOkResponse } from "@nestjs/swagger";
 
 @Controller()
 export class UsersController {
   constructor(private usersService: UsersService) {}
 
+  @ApiOperation({ summary: "Returns a list of users in the database." })
+  @ApiOkResponse({
+    description: "List of users: ID, name and email.",
+    schema: {
+      example: [
+        { id: 1, name: "Bob Bobby", email: "bobbobby@example.com" },
+        { id: 2, name: "Rob Robby", email: "robrobby@example.com" },
+        { id: 3, name: "Tob Tobby", email: "tobtobby@example.com" },
+      ],
+    },
+  })
   @Get()
   findAll() {
     return this.usersService.findAll();
