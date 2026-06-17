0bb9ff37e195519a05bb4f941a2a6714cf12f6a1
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-06-17 12:18:46 +0200

    feat: repository pattern for signup endpoint

--- src/users/users.repository.ts
+++ src/users/users.repository.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
-import { PrismaService } from '../prisma/prisma.service';
+import { Injectable } from "@nestjs/common";
+import { PrismaService } from "../prisma/prisma.service";
+import { users } from "../generated/prisma/client";
 
 @Injectable()
 export class UsersRepository {
@@ -7,5 +8,21 @@ export class UsersRepository {
 
   async findAll() {
     return this.prisma.users.findMany();
+  }
+
+  async findByEmail(email: string): Promise<users | null> {
+    return this.prisma.users.findUnique({
+      where: { email },
+    });
+  }
+
+  async create(data: {
+    email: string;
+    name: string;
+    password: string;
+  }): Promise<users> {
+    return this.prisma.users.create({
+      data,
+    });
   }
 }
