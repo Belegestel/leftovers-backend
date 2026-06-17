fb76e632c6a2373850053f9be092d254bdab3543
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-06-17 09:21:12 +0200

    chore: improved Swagger docs

--- src/app.controller.ts
+++ src/app.controller.ts
@@ -1,14 +1,17 @@
-import { ApiOperation, ApiResponse } from '@nestjs/swagger';
-import { Controller, Get } from '@nestjs/common';
-import { AppService } from './app.service';
+import { ApiOperation, ApiOkResponse } from "@nestjs/swagger";
+import { Controller, Get } from "@nestjs/common";
+import { AppService } from "./app.service";
 
 @Controller()
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @ApiOperation({ summary: 'Check if the server is running.' })
-  @ApiResponse({ status: 200, description: 'Server is running.' })
-  @Get('health')
+  @ApiOperation({ summary: "Check if the server is running." })
+  @ApiOkResponse({
+    description: "Server is running.",
+    example: "OK",
+  })
+  @Get("health")
   getHealth(): string {
     return "OK";
   }
