530948055e991a521a59f691358fd34a7ff0a888
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-06-22 08:36:44 +0200

    feat: initial email setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,10 @@
-import { ConfigModule } from '@nestjs/config';
-import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-import { UsersModule } from './users/users.module';
-import { AuthModule } from './auth/auth.module';
+import { ConfigModule } from "@nestjs/config";
+import { Module } from "@nestjs/common";
+import { AppController } from "./app.controller";
+import { AppService } from "./app.service";
+import { UsersModule } from "./users/users.module";
+import { AuthModule } from "./auth/auth.module";
+// import { EmailModule } from './email/email.module';
 
 @Module({
   imports: [
@@ -11,7 +12,8 @@ import { AuthModule } from './auth/auth.module';
       isGlobal: true,
     }),
     UsersModule,
-    AuthModule
+    AuthModule,
+    // EmailModule,
   ],
   controllers: [AppController],
   providers: [AppService],
