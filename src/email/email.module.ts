e32ee4c5238b43fd16290618ecf35454f300f544
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-06-22 12:08:30 +0200

    feat: initial mail setup (WIP)

---
+++ src/email/email.module.ts
@@ -0,0 +1,41 @@
+import { Module } from "@nestjs/common";
+import { MailerModule } from "@nestjs-modules/mailer";
+import { HandlebarsAdapter } from "@nestjs-modules/mailer/adapters/handlebars.adapter";
+import { EmailService } from "./email.service";
+import { join } from "path";
+import { ConfigModule, ConfigService } from "@nestjs/config";
+
+@Module({
+  imports: [
+    ConfigModule,
+    MailerModule.forRootAsync({
+      imports: [ConfigModule],
+      inject: [ConfigService],
+      useFactory: async (config: ConfigService) => {
+        return {
+          transport: {
+            host: "smtp.ethereal.email",
+            port: 587,
+            auth: {
+              user: config.get<string>("EMAIL_USER"),
+              pass: config.get<string>("EMAIL_PASS"),
+            },
+          },
+          defaults: {
+            from: '"No Reply" <no-reply@example.com>',
+          },
+          template: {
+            dir: join(__dirname, "templates"),
+            adapter: new HandlebarsAdapter(),
+            options: {
+              strict: true,
+            },
+          },
+        };
+      },
+    }),
+  ],
+  providers: [EmailService],
+  exports: [EmailService],
+})
+export class EmailModule {}
---
+++ src/email/email.service.spec.ts
@@ -0,0 +1,37 @@
+import { Test, TestingModule } from "@nestjs/testing";
+import { MailerService } from "@nestjs-modules/mailer";
+import { EmailService } from "./email.service";
+
+describe("EmailService", () => {
+  let service: EmailService;
+
+  const mailerMock = {
+    sendMail: jest.fn().mockReturnValue(true),
+  };
+
+  beforeEach(async () => {
+    const module: TestingModule = await Test.createTestingModule({
+      providers: [
+        EmailService,
+        { provide: MailerService, useValue: mailerMock },
+      ],
+    }).compile();
+
+    service = module.get<EmailService>(EmailService);
+  });
+
+  it("should be defined", () => {
+    expect(service).toBeDefined();
+  });
+
+  it("should send email", async () => {
+    await service.sendEmail(
+      "test@test.com",
+      "Test",
+      "example",
+      { name: "John", email: "test@test.com" },
+    );
+
+    expect(mailerMock.sendMail).toHaveBeenCalled();
+  });
+});
