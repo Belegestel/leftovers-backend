45329ef83fd4821c3f064d6507bee48d86cf92d3
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-06-23 13:57:09 +0200

    feat: added unit tests for registration verification backends

--- src/auth/auth.service.spec.ts
+++ src/auth/auth.service.spec.ts
@@ -13,6 +13,7 @@ import { SignupRequestsRepository } from "./signup-requests.repository";
 import { mockSignupRequestsRepository } from "../../test/unit/mocks/mockSignupRequestsRepository";
 import { mockEmailService } from "../../test/unit/mocks/mockEmailService";
 import { EmailService } from "../email/email.service";
+import { warn } from "console";
 
 jest.mock("bcrypt", () => ({
   hash: jest.fn(),
@@ -33,8 +34,11 @@ describe("AuthService", () => {
         { provide: UsersRepository, useValue: mockUsersRepository },
         { provide: JwtService, useValue: mockJwtService },
         { provide: PrismaService, useValue: mockPrismaService },
-        { provide: SignupRequestsRepository, useValue: mockSignupRequestsRepository },
-        { provide: EmailService, useValue: mockEmailService}
+        {
+          provide: SignupRequestsRepository,
+          useValue: mockSignupRequestsRepository,
+        },
+        { provide: EmailService, useValue: mockEmailService },
       ],
       imports: [
         ConfigModule.forRoot({
@@ -51,53 +55,95 @@ describe("AuthService", () => {
     jest.clearAllMocks();
   });
 
-  describe("signup", () => {
-    it("should create a new user", async () => {
+  describe("register", () => {
+    it("should create a signup request and send email", async () => {
       const dto: SignupDto = {
         email: "john.doe@email.com",
-        password: "password",
         name: "John Doe",
+        password: "password123",
       };
 
       mockUsersRepository.findByEmail.mockResolvedValue(null);
+      mockSignupRequestsRepository.findByEmail.mockResolvedValue(null);
+
       (bcrypt.hash as jest.Mock).mockResolvedValue("hashed-password");
-      mockUsersRepository.create.mockResolvedValue({
+      mockSignupRequestsRepository.create.mockResolvedValue({
         id: 1,
         email: dto.email,
-        name: dto.name,
-        password: "hashed-password",
       });
 
-      const result = await service.signup(dto);
-      expect(mockUsersRepository.findByEmail).toHaveBeenCalledWith(
-        "john.doe@email.com",
+      const res = await service.register(dto);
+
+      expect(mockUsersRepository.findByEmail).toHaveBeenCalledWith(dto.email);
+      expect(mockSignupRequestsRepository.findByEmail).toHaveBeenCalledWith(
+        dto.email,
       );
-      expect(bcrypt.hash).toHaveBeenCalledWith("password", 12);
-      expect(mockUsersRepository.create).toHaveBeenCalledWith({
-        email: "john.doe@email.com",
-        password: "hashed-password",
-        name: "John Doe",
-      });
-      expect(result).toEqual({
-        id: 1,
-        email: "john.doe@email.com",
+      expect(bcrypt.hash).toHaveBeenCalledWith("password123", 12);
+      expect(mockSignupRequestsRepository.create).toHaveBeenCalled();
+      expect(res).toEqual({
+        message: "Confirmation email sent.",
       });
     });
 
     it("should throw ConflictException if email already exists", async () => {
       const dto: SignupDto = {
         email: "john.doe@email.com",
-        password: "password",
         name: "John Doe",
+        password: "password123",
       };
 
-      mockUsersRepository.findByEmail.mockResolvedValue({
-        id: 1,
+      mockUsersRepository.findByEmail.mockResolvedValue({ id: 1 });
+      mockSignupRequestsRepository.findByEmail.mockResolvedValue(null);
+
+      await expect(service.register(dto)).rejects.toThrow(ConflictException);
+      expect(mockSignupRequestsRepository.create).not.toHaveBeenCalled();
+    });
+  });
+
+  describe("confirmRegistration", () => {
+    it("should create user and delete signup requests", async () => {
+      const dto = {
+        email: "john.doe@email.com",
+        token: "valid-token",
+      };
+      mockSignupRequestsRepository.findByEmail.mockResolvedValue({
         email: dto.email,
+        token: dto.token,
+        expires_at: new Date(Date.now() + 10000),
+        name: "John Doe",
+        password_hash: "hashed-password",
+        id: 1,
+      });
+      mockUsersRepository.create.mockResolvedValue({ id: 1, email: dto.email });
+
+      const result = await service.confirmRegistration(dto);
+      expect(mockSignupRequestsRepository.deleteById).toHaveBeenCalledWith(1);
+      expect(result).toEqual({ id: 1, email: dto.email });
+    });
+
+    it("should throw error for invalid token", async () => {
+      const dto = { email: "john.doe@email.com", token: "wrong-token" };
+      mockSignupRequestsRepository.findByEmail.mockResolvedValue({
+        email: dto.email,
+        token: "valid-token",
+        expires_at: new Date(Date.now() + 10000),
+        name: "John Doe",
+        password_hash: "hashed-password",
       });
 
-      await expect(service.signup(dto)).rejects.toThrow(ConflictException);
-      expect(mockUsersRepository.create).not.toHaveBeenCalled();
+      await expect(service.confirmRegistration(dto)).rejects.toThrow();
+    });
+
+    it("should throw error for expired token", async () => {
+      const dto = { email: "john.doe@email.com", token: "valid-token" };
+      mockSignupRequestsRepository.findByEmail.mockResolvedValue({
+        email: dto.email,
+        token: dto.token,
+        expires_at: new Date(Date.now() - 10000),
+        name: "John Doe",
+        password_hash: "hashed-password",
+      });
+      await expect(service.confirmRegistration(dto)).rejects.toThrow();
     });
   });
 
