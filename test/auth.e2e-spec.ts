291e88e9db4c9b6a1e3070f99ad657a198f3803f
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-06-24 08:48:40 +0200

    feat: e2e tests for the /auth/register and /auth/confirm-registration endpoints

--- test/auth.e2e-spec.ts
+++ test/auth.e2e-spec.ts
@@ -23,24 +23,9 @@ describe("Auth E2E", () => {
     await app.close();
   });
 
-  it("/auth/signup POST should create a user", async () => {
-    const testEmail = `john.doe${randomUUID()}@email.com`;
-    const response = await request(app.getHttpServer())
-      .post("/auth/signup")
-      .send({
-        email: testEmail,
-        password: "password",
-        name: "John Doe",
-      })
-      .expect(201);
-
-    expect(response.body).toHaveProperty("id");
-    expect(response.body.email).toBe(testEmail);
-  });
-
-  it("should return 400 for invalid email", async () => {
+  it("/auth/register should return 400 for invalid email", async () => {
     await request(app.getHttpServer())
-      .post("/auth/signup/")
+      .post("/auth/register")
       .send({
         email: "a",
         password: "password",
@@ -49,9 +34,9 @@ describe("Auth E2E", () => {
       .expect(400);
   });
 
-  it("should return 400 if the name is missing", async () => {
+  it("/auth/register/ should return 400 if the name is missing", async () => {
     await request(app.getHttpServer())
-      .post("/auth/signup/")
+      .post("/auth/register/")
       .send({
         email: `john.doe${randomUUID()}@email.com`,
         password: "password",
@@ -59,50 +44,51 @@ describe("Auth E2E", () => {
       .expect(400);
   });
 
-  it("/auth/login/ POST should return JWT for valid credentials", async () => {
-    const testEmail = `john.doe${randomUUID()}@email.com`;
-    const password = "password";
-    await request(app.getHttpServer())
-      .post("/auth/signup")
-      .send({ email: testEmail, password: password, name: "John Doe" })
-      .expect(201);
+  it("/auth/login POST should return 401 for wrong password", async () => {
+    const email = `john.doe${randomUUID()}@email.com`;
+    const password = `password${randomUUID()}`;
 
-    const response = await request(app.getHttpServer())
-      .post("/auth/login")
-      .send({ email: testEmail, password: password })
+    await request(app.getHttpServer())
+      .post("/auth/register")
+      .send({ email, password, name: "John Doe" })
       .expect(200);
 
-    expect(response.body).toHaveProperty("accessToken");
-    expect(typeof response.body.accessToken).toBe("string");
-  });
+    const signupRequest = await prisma.signup_requests.findUnique({
+      where: { email },
+    });
 
-  it("/auth/login POST should return 401 for wrong password", async () => {
-    const testEmail = `john.doe${randomUUID()}@email.com`;
-    const password = "password";
-    const differentPassword = password + "123";
     await request(app.getHttpServer())
-      .post("/auth/signup")
-      .send({ email: testEmail, password: password, name: "John Doe" })
+      .post("/auth/confirm-registration")
+      .send({ email, token: signupRequest!.token })
       .expect(201);
 
     await request(app.getHttpServer())
       .post("/auth/login")
-      .send({ email: testEmail, password: differentPassword })
+      .send({ email, password: "wrong-password" })
       .expect(401);
   });
 
   it("/auth/login POST should return 401 for wrong email", async () => {
-    const testEmail = `john.doe${randomUUID()}@email.com`;
-    const password = "password";
-    const missingEmail = "missing@email.com";
+    const email = `john.doe${randomUUID()}@email.com`;
+    const password = `password${randomUUID()}`;
+
     await request(app.getHttpServer())
-      .post("/auth/signup")
-      .send({ email: testEmail, password: password, name: "John Doe" })
+      .post("/auth/register")
+      .send({ email, password, name: "John Doe" })
+      .expect(200);
+
+    const signupRequest = await prisma.signup_requests.findUnique({
+      where: { email },
+    });
+
+    await request(app.getHttpServer())
+      .post("/auth/confirm-registration")
+      .send({ email, token: signupRequest!.token })
       .expect(201);
 
     await request(app.getHttpServer())
       .post("/auth/login")
-      .send({ email: missingEmail, password: password })
+      .send({ email: "1" + email, password })
       .expect(401);
   });
 
@@ -111,5 +97,104 @@ describe("Auth E2E", () => {
       .post("/auth/login")
       .send({ email: "john.doe[not]email.com" })
       .expect(400);
+  });
+
+  it("/auth/register POST should create a signup request", async () => {
+    const email = `john.doe${randomUUID()}@email.com`;
+    const response = await request(app.getHttpServer())
+      .post("/auth/register")
+      .send({
+        email,
+        password: "password",
+        name: "John Doe",
+      })
+      .expect(200);
+
+    expect(response.body).toEqual({ message: "Confirmation email sent." });
+    const signupRequest = await prisma.signup_requests.findUnique({
+      where: { email },
+    });
+
+    expect(signupRequest).not.toBeNull();
+    expect(signupRequest?.email).toBe(email);
+    expect(signupRequest?.token).toBeTruthy();
+  });
+
+  it("/auth/register POST should reject dupicate email", async () => {
+    const email = `john.doe${randomUUID()}@email.com`;
+
+    await request(app.getHttpServer())
+      .post("/auth/register")
+      .send({ email, password: "password", name: "John Doe" })
+      .expect(200);
+    await request(app.getHttpServer())
+      .post("/auth/register")
+      .send({ email, password: "password", name: "John Doe" })
+      .expect(409);
+  });
+
+  it("/auth/confirm-registration POST should create user", async () => {
+    const email = `john.doe${randomUUID()}@email.com`;
+    await request(app.getHttpServer())
+      .post("/auth/register")
+      .send({ email, password: "password", name: "John Doe" })
+      .expect(200);
+
+    const signupRequest = await prisma.signup_requests.findUnique({
+      where: { email },
+    });
+
+    expect(signupRequest).not.toBeNull();
+
+    const response = await request(app.getHttpServer())
+      .post("/auth/confirm-registration")
+      .send({ email, token: signupRequest!.token })
+      .expect(201);
+
+    expect(response).not.toBeNull();
+    expect(response.body.email).toBe(email);
+
+    const user = await prisma.users.findUnique({ where: { email } });
+    expect(user).not.toBeNull();
+  });
+
+  it("/auth/confirm-registration POST should reject invalid token", async () => {
+    const email = `john.doe${randomUUID()}@email.com`;
+
+    await request(app.getHttpServer())
+      .post("/auth/register")
+      .send({ email, password: "password", name: "John Doe" })
+      .expect(200);
+
+    await request(app.getHttpServer())
+      .post("/auth/confirm-registration")
+      .send({ email, token: "invalid-token" })
+      .expect(400);
+  });
+
+  it("/auth/register, /auth/confirm-registration, /auth/login should register the user and log them in", async () => {
+    const email = `john.doe${randomUUID()}@email.com`;
+    const password = `password${randomUUID()}`;
+
+    await request(app.getHttpServer())
+      .post("/auth/register")
+      .send({ email, password, name: "John Doe" })
+      .expect(200);
+
+    const signupRequest = await prisma.signup_requests.findUnique({
+      where: { email },
+    });
+
+    await request(app.getHttpServer())
+      .post("/auth/confirm-registration")
+      .send({ email, token: signupRequest!.token })
+      .expect(201);
+
+    const response = await request(app.getHttpServer())
+      .post("/auth/login")
+      .send({ email, password })
+      .expect(200);
+
+    expect(response.body).toHaveProperty("accessToken");
   });
 });
