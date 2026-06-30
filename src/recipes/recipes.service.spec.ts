07f5f8ea92968dac0c7fbb49f98117263046d67c
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-06-30 10:59:01 +0200

    fix: mocked recipesRepository instead of the prismaService in recipes service unit tests

--- src/recipes/recipes.service.spec.ts
+++ src/recipes/recipes.service.spec.ts
@@ -1,19 +1,16 @@
 import { Test, TestingModule } from "@nestjs/testing";
 import { RecipesService } from "./recipes.service";
-import { mockPrismaService } from "../../test/unit/mocks/mockPrismaService";
-import { PrismaService } from "../prisma/prisma.service";
 import { RecipesRepository } from "./recipes.repository";
+import { mockRecipesRepository } from "../../test/unit/mocks/mockRecipesRepository";
 
 describe("RecipesService", () => {
   let service: RecipesService;
-  let prismaMock = mockPrismaService;
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       providers: [
         RecipesService,
-        { provide: PrismaService, useValue: prismaMock },
-        RecipesRepository,
+        { provide: RecipesRepository, useValue: mockRecipesRepository },
       ],
     }).compile();
 
@@ -23,105 +20,65 @@ describe("RecipesService", () => {
 
   describe("findAll", () => {
     it("returns only public recipes for guests", async () => {
-      prismaMock.recipe.findMany.mockResolvedValue([]);
       await service.findAll();
 
-      expect(prismaMock.recipe.findMany).toHaveBeenCalledWith({
-        where: {
-          AND: [{ isPublic: true }],
-        },
-        orderBy: {
-          created_at: "desc",
-        },
-      });
+      expect(mockRecipesRepository.findAll).toHaveBeenCalledWith(
+        undefined,
+        expect.objectContaining({ userId: undefined }),
+      );
     });
 
     it("returns public recipes and own private recipes for logged in users", async () => {
-      prismaMock.recipe.findMany.mockResolvedValue([]);
-
       await service.findAll(1);
 
-      expect(prismaMock.recipe.findMany).toHaveBeenCalledWith({
-        where: {
-          AND: [{ OR: [{ isPublic: true }, { author_id: 1 }] }],
-        },
-        orderBy: {
-          created_at: "desc",
-        },
-      });
+      expect(mockRecipesRepository.findAll).toHaveBeenCalledWith(
+        1,
+        expect.objectContaining({ userId: 1 }),
+      );
     });
 
     it("applies category filtering", async () => {
-      prismaMock.recipe.findMany.mockResolvedValue([]);
       await service.findAll(undefined, {
         category: "Dessert,Dinner",
       } as any);
 
-      expect(prismaMock.recipe.findMany).toHaveBeenCalledWith(
-        expect.objectContaining({
-          where: {
-            AND: expect.arrayContaining([
-              {
-                category: {
-                  in: ["Dessert", "Dinner"],
-                },
-              },
-            ]),
-          },
-        }),
+      expect(mockRecipesRepository.findAll).toHaveBeenCalledWith(
+        undefined,
+        expect.objectContaining({ category: "Dessert,Dinner" }),
       );
     });
 
     it("applies rating filter", async () => {
-      prismaMock.recipe.findMany.mockResolvedValue([]);
       await service.findAll(undefined, {
         rating: 4,
       });
 
-      expect(prismaMock.recipe.findMany).toHaveBeenCalledWith(
-        expect.objectContaining({
-          where: { AND: expect.arrayContaining([{ rating: { gte: 4 } }]) },
-        }),
+      expect(mockRecipesRepository.findAll).toHaveBeenCalledWith(
+        undefined,
+        expect.objectContaining({ rating: 4 }),
       );
     });
 
     it("applies title search", async () => {
-      prismaMock.recipe.findMany.mockResolvedValue([]);
       await service.findAll(undefined, {
         title: "cake",
       } as any);
 
-      expect(prismaMock.recipe.findMany).toHaveBeenCalledWith(
-        expect.objectContaining({
-          where: {
-            AND: expect.arrayContaining([
-              { OR: [{ title: { contains: "cake", mode: "insensitive" } }] },
-            ]),
-          },
-        }),
+      expect(mockRecipesRepository.findAll).toHaveBeenCalledWith(
+        undefined,
+        expect.objectContaining({ title: "cake" }),
       );
     });
 
     it("applies multiple search conditions", async () => {
-      prismaMock.recipe.findMany.mockResolvedValue([]);
       await service.findAll(undefined, {
         title: "cake",
         ingredients: "flour",
       } as any);
 
-      expect(prismaMock.recipe.findMany).toHaveBeenCalledWith(
-        expect.objectContaining({
-          where: {
-            AND: expect.arrayContaining([
-              {
-                OR: [
-                  { title: { contains: "cake", mode: "insensitive" } },
-                  { ingredients: { contains: "flour", mode: "insensitive" } },
-                ],
-              },
-            ]),
-          },
-        }),
+      expect(mockRecipesRepository.findAll).toHaveBeenCalledWith(
+        undefined,
+        expect.objectContaining({ title: "cake", ingredients: "flour" }),
       );
     });
   });
---
+++ test/unit/mocks/mockRecipesRepository.ts
@@ -0,0 +1,3 @@
+export const mockRecipesRepository = {
+  findAll: jest.fn(),
+}
