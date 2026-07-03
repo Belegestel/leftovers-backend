92ecfc2c43cdfa2b1830d6e9ea98a1c94b7e324d
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-07-03 10:37:40 +0200

    feat: file upload service unit tests

--- src/files/files.controller.spec.ts
+++ src/files/files.controller.spec.ts
@@ -1,18 +1,43 @@
-import { Test, TestingModule } from '@nestjs/testing';
-import { FilesController } from './files.controller';
+import { Test, TestingModule } from "@nestjs/testing";
+import { FilesController } from "./files.controller";
+import { JwtAuthGuard } from "../auth/jwt-auth.guard";
+import { mockFilesService } from "../../test/unit/mocks/mockFilesService";
+import { FilesService } from "./files.service";
 
-describe('FilesController', () => {
+describe("FilesController", () => {
   let controller: FilesController;
+  let service: FilesService;
+
+  const jwtAuthGuardMock = { canActivate: jest.fn(() => true) };
 
   beforeEach(async () => {
     const module: TestingModule = await Test.createTestingModule({
       controllers: [FilesController],
-    }).compile();
+      providers: [{ provide: FilesService, useValue: mockFilesService }],
+    })
+      .overrideGuard(JwtAuthGuard)
+      .useValue(jwtAuthGuardMock)
+      .compile();
 
     controller = module.get<FilesController>(FilesController);
+    service = module.get<FilesService>(FilesService);
   });
 
-  it('should be defined', () => {
-    expect(controller).toBeDefined();
+  it("should call service and return response DTO", async () => {
+    const dto = {
+      fileName: "image.png",
+      fileType: "image/png",
+      folder: "images",
+    };
+    const serviceResult = {
+      url: "https://signed-url.com",
+      key: "images/uuid.png",
+    };
+    mockFilesService.createPresignedUploadUrl.mockResolvedValue(serviceResult);
+
+    const result = await controller.createPresignedUrl(dto);
+
+    expect(service.createPresignedUploadUrl).toHaveBeenCalled();
+    expect(result).toEqual(serviceResult);
   });
 });
---
+++ test/unit/mocks/mockFilesService.ts
@@ -0,0 +1,3 @@
+export const mockFilesService = {
+  createPresignedUploadUrl: jest.fn(),
+};
