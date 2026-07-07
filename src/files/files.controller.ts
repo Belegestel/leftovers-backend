58ab59181969d5728a825ea876d0f1d80f1248ac
Wojciech Dudziński <wojdud.pl@gmail.com>
2026-07-07 09:42:55 +0200

    chore: removed previous files controller endpoint

--- src/files/files.controller.ts
+++ src/files/files.controller.ts
@@ -1,26 +1,7 @@
-import { Body, Controller, Post, UseGuards } from "@nestjs/common";
+import { Controller } from "@nestjs/common";
 import { FilesService } from "./files.service";
-import { PresignedUrlRequest } from "./dto/requests/presignedUrlRequest.dto";
-// import { CreatePresignedUrl } from "./dto/createPresignedUrl.dto";
-import { PresignedUrlResponse } from "./dto/responses/presignedUrlResponse.dto";
-import { JwtAuthGuard } from "../auth/jwt-auth.guard";
-import { randomUUID } from "crypto";
 
 @Controller("files")
 export class FilesController {
   constructor(private readonly filesService: FilesService) {}
-
-  // @UseGuards(JwtAuthGuard)
-  // @Post("presigned-url")
-  async createPresignedUrl(
-    @Body() dto: PresignedUrlRequest,
-  ): Promise<PresignedUrlResponse> {
-    // const input = CreatePresignedUrl.from(dto); // TODO to remove
-    const result = await this.filesService.createPresignedUploadUrl(
-      `${randomUUID()}-${dto.fileName}`,
-      dto.fileType,
-    );
-    const response = PresignedUrlResponse.from(result);
-    return response;
-  }
 }
